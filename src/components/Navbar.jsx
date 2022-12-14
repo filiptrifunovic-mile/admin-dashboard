8880c76ecc0d61ffbc8dfba178d17e86a038d2b5
Filip <filip.trifunovic23@gmail.com>
2022-12-14 20:13:27 +0100

    ecommerce 1st part

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,9 +23,8 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
       <span
         className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
         style={{ background: dotColor }}
-      >
-        {icon}
-      </span>
+      />
+      {icon}
     </button>
   </TooltipComponent>
 );
